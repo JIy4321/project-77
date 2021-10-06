5bd6ca8919e1c416e125e1a0d2705bcb65533493
JIy4321 <90255079+JIy4321@users.noreply.github.com>
2021-10-06 12:44:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("This is my first console project.");
+var first_name="Jiya ";
+var last_name="Patel";
+console.log(first_name.concat(last_name));
+var a=10;
+var b=20;
+var add=a+b;
+console.log(add);
+var c=25;
+var d=30;
+var subtract=d-c;
+console.log(subtract)
+var multiplication=c*d;
+console.log(multiplication);
